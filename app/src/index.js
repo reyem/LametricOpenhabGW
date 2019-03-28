2321d136a463e2fd2d35d9e7479118e8ccb6838d
Marco Meyer <marco.g.meyer@gmail.com>
2019-03-28 23:54:42 +0100

    made apps configurable

--- app/src/index.js
+++ app/src/index.js
@@ -8,13 +8,7 @@ var configFile = '../../etc/lametricopenhabgw/config.json'
 if (process.argv.length > 2) {
     configFile = process.argv[2];
 }
-
 const config = require(configFile);
-var openhabHost = config.openhabHost;
-var openhabPort = config.openhabPort;
-
-const itemFinderRegexp = /\$\{([A-Za-z0-9_-]+)\}/g;
-
 
 async function asyncForEach(array, callback) {
     for (let index = 0; index < array.length; index++) {
@@ -22,10 +16,9 @@ async function asyncForEach(array, callback) {
     }
 }
 
-
 function callOpenHabAPI(itemName) {
     var options = {
-        url: 'http://' + openhabHost + ':' + openhabPort + '/rest/items/' + itemName,
+        url: 'http://' + config.openhabHost + ':' + config.openhabPort + '/rest/items/' + itemName,
         headers: {
             'User-Agent': 'request'
         }
@@ -46,24 +39,27 @@ function callOpenHabAPI(itemName) {
 
 app.use(nocache())
 
-app.get('/tempIndicator', function (req, res) {
-    const execute = async () => {
-        var response = config.pollingApps.tempIndicator.responseTemplate;
-        var responseStr = JSON.stringify(response)
-        
-        var items = responseStr.match(itemFinderRegexp)
-        await asyncForEach(items, async (item) => {
-            item = item.substring(2, item.length -1)
-            await callOpenHabAPI(item).then(function (result) {                         
-                responseStr = responseStr.replace(new RegExp('\\$\\{' + item + '\\}'), result.state)
-            }, function (err) {
-                console.log(err);
+for(var pollingApp in config.pollingApps ) {
+    console.log('deploy app /' + pollingApp)
+    app.get('/' + pollingApp, function (req, res) {
+        const execute = async () => {
+            const itemFinderRegexp = /\$\{([A-Za-z0-9_-]+)\}/g;
+            var response = config.pollingApps[pollingApp];            
+            var responseStr = JSON.stringify(response)            
+            var items = responseStr.match(itemFinderRegexp)
+            await asyncForEach(items, async (item) => {
+                item = item.substring(2, item.length -1)
+                await callOpenHabAPI(item).then(function (result) {                         
+                    responseStr = responseStr.replace(new RegExp('\\$\\{' + item + '\\}'), result.state)
+                }, function (err) {
+                    console.log(err);
+                })
             })
-        })
+            res.send(JSON.parse(responseStr));
+        }
+        execute()
+    })
+}
 
-        res.send(JSON.parse(responseStr));
-    }
-    execute()
-})
 
 app.listen(3000)
